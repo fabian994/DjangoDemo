3afbb53d697a062247c20c99c36677410f95119a
CanmingCai <caicanming50@gmail.com>
2023-05-15 17:01:47 -0600

    Tabla SQL

--- DjangoDemo/demoCR/models.py
+++ DjangoDemo/demoCR/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):    
+    matricula = models.CharField(max_length=10, primary_key=True, unique=True)
+    name = models.CharField(max_length=30)    
+    age = models.IntegerField()
